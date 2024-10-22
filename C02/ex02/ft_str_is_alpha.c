1c3b70e3ba37d27c05b3786be166fb1d24e62138
Daniel Alvarez <danipoal.0@gmail.com>
2024-10-22 23:38:27 +0200

    prototype isalpha

---
+++ C02/ex02/ft_str_is_alpha.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void	ft_str_is_alpha(char *str)
+{
+	int	count;
+	int	isalpha;
+
+	count = 0;
+	isalpha = 0;
+	while(str != '\0')
+	{
+		if(str[i] >= ss)
+	}
+}
+
+int	main(void)
+{
+	char	str[5];
+
+	str = "Hol)";
+	ft_str_is_alpha(char *str)
+
+}
