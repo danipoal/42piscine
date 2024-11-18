d6083366b6090a528930ddffca7b320b6b6f3067
Daniel Alvarez <danipoal.0@gmail.com>
2024-11-18 18:04:48 +0100

    Display_file like cat

--- C10/ex00/main.c
+++ C10/ex00/main.c
@@ -4,12 +4,21 @@
 int ft_display_file(char *url)
 {
     int fd;
+    char buffer[1024];
+    int readed_bytes;
+    int i;
 
     fd = open(url, O_RDONLY);
     if (fd < 0)
         return (0);
-    
-    
+    readed_bytes = read(fd, buffer, 1024);
+    if (readed_bytes < 0)
+        return (0);
+    buffer[readed_bytes] = '\0';
+    i = 0;
+    while (i <= readed_bytes)
+        write(1, &buffer[i++], 1);
+    return (1);
 }
 
 int main(int ac, char **av)
@@ -19,19 +28,18 @@ int main(int ac, char **av)
         write(1, "File name missing.\n", 19);
         return (1);
     }
-    else if (ac < 2)
+    else if (ac > 2)
     {
         write(1, "Too many arguments.\n", 20);
         return (1);
     }
     else if(ac == 2)
     {
-        if (!ft_display_file(av[1]));
+        if (!ft_display_file(av[1]))
         {
             write(1, "Cannot read file.\n", 18);
             return (1);
         }
     }
-    
     return (0);
 }
